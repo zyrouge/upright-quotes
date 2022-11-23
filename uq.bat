bd926608463720f2018c5580c2a1f7edbaf1b838
zyrouge <naveencnaveen96@gmail.com>
2022-11-23 20:14:13 +0530

    chore: uq

--- uq.bat
+++ uq.bat
@@ -7,8 +7,10 @@ if "%1" == "add" (
 	cmd /C git add backend/data/quotes.json
 	cmd /C git commit -m "feat: new quote"
 	cmd /C git push
+) else if "%1" == "deploy" (
+	cmd /C gh workflow run deploy
 ) else if "%1" == "" (
-	echo Available Commands: add, push.
+	echo Available Commands: add, push, deploy.
 ) else (
 	echo Unknown Command: %1!
 )
